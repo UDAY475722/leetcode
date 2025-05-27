001ecb50825d200cf470d349809d21f8d5f6b310
UDAY475722 <139937498+UDAY475722@users.noreply.github.com>
2025-05-27 11:08:16 +0530

    Time: 1512 ms (5.08%), Space: 40.5 MB (54.05%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
         if(num<2)return true;
-        if(num<101){for(int i=2;i<=(num/2)+1;i++){
+        if(num<501){for(int i=2;i<=(num/2)+1;i++){
             if(i*i==num) return true;
             }
         }
-        else if(num>=101){
-        for(int i=2;i<=(num/10)+1;i++){
+        else if(num>=501){
+        for(int i=2;i<=(num/5)+1;i++){
             if(i*i==num) return true;
         }
         }
